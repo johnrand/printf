f66cab9062b532b14b973e09e68a266f857d0fd2
johnrand <johnrandy0652@gmail.com>
2023-05-02 10:58:55 -0700

    fix

--- printf.c
+++ printf.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
-#include <stdlib>
-
 /**
  * _printf - produces output according to a format
  * @format: character string containing zero or more directives
